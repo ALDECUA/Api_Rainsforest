643fb94c60630e3b28ea8c635b7f9663a3169f8e
ALDECUA <cruz.aldej@gmail.com>
2022-11-06 23:55:31 -0600

    Retro

--- src/modules/index.js
+++ src/modules/index.js
@@ -8,13 +8,7 @@
 const index = require("./default");
 const auth = require("../auth/index");
 const users = require("./users/index");
-
-
-
-/* const producto 			=	require("./productos/index");*/
-
 const login = require("./login");
-
 const crm_usuarios = require('./crm_usuarios');
 const estadisticas = require('./estadisticas');
 const crm_reportes = require('./crm_reportes');
@@ -25,15 +19,10 @@ module.exports = {
     modules: [
         index.routes(),
         login.routes(),
-       
-       
         users.routes(),
         estadisticas.routes(),
-      
         crm_usuarios.routes(),
-       
         crm_reportes.routes(),
         admin.routes(),
-     
     ]
 };
