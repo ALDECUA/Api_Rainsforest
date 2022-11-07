fb66c4a0d425d58e63b7ce4a7620827fb8e79a42
ALDECUA <cruz.aldej@gmail.com>
2022-11-06 23:54:44 -0600

    limpieza 10.0.1

--- src/modules/index.js
+++ src/modules/index.js
@@ -8,49 +8,32 @@
 const index = require("./default");
 const auth = require("../auth/index");
 const users = require("./users/index");
-const pais = require("./pais/index");
-const modulos = require("./modulos/index");
-const roles = require("./Roles/index");
+
+
+
 /* const producto 			=	require("./productos/index");*/
-const movimiento = require("./movimiento/index");
-const resumenes = require("./resumenes/index");
-const notificaciones = require("./notificaciones/index");
-const paquete = require("./paquete/index");
-const regimen = require("./regimen/index");
-const unidades = require("./unidades/index");
-const transformaciones = require("./transformaciones/index");
+
 const login = require("./login");
-const lotes = require('./lotes');
-const estadisticas = require('./estadisticas');
-const referidos = require('./referidos');
-const webapp = require('./webapp');
-const rd = require('./rd');
-const pedidos = require('./pedidos');
+
 const crm_usuarios = require('./crm_usuarios');
-const villamar = require('./villamar');
+const estadisticas = require('./estadisticas');
 const crm_reportes = require('./crm_reportes');
 const admin = require('./admin')
-const zapprospectos = require('./zap_prospectos');
-//Cambio 28/06/21
-const reclutamiento = require('./reclutamiento');
+
 
 module.exports = {
     modules: [
         index.routes(),
         login.routes(),
-        unidades.routes(),
-        lotes.routes(),
-        reclutamiento.routes(),
+       
+       
         users.routes(),
         estadisticas.routes(),
-        referidos.routes(),
-        webapp.routes(),
-        pedidos.routes(),
-        rd.routes(),
+      
         crm_usuarios.routes(),
-        villamar.routes(),
+       
         crm_reportes.routes(),
         admin.routes(),
-        zapprospectos.routes()
+     
     ]
 };
